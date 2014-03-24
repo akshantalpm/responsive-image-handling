30a6d0524e03d5b9b9035953f824161b0b13db71
Pooja Akshantal <pooja.akshantal@gmail.com>
2014-03-24 18:01:55 +0530

    Fixing jasmine test

--- jasmine/spec/ResponsiveImageSpec.js
+++ jasmine/spec/ResponsiveImageSpec.js
@@ -1,79 +1,53 @@
-define(['ri'], function () {
-
-  describe("ResponsiveImageHandler", function () {
-
-    it("loads images with default settings", function () {
-      window.devicePixelRatio = 0.4;
-      var input = document.createElement('div');
-      input.innerHTML = '<img class="ri" data-desktop-width="700" data-tablet-width="400" data-phone-width="200" \
+describe("ResponsiveImageHandler", function () {
+  var input = document.createElement('div');
+  input.setAttribute("style", "display: none;");
+  input.innerHTML = '<img class="ri" data-desktop-width="700" data-tablet-width="400" data-phone-width="200" \
           data-src-desktop="http://res.cloudinary.com/demo/image/upload/sample.jpg"\
           data-src-tablet="http://res.cloudinary.com/demo/image/upload/sample.jpg"\
           data-src-phone="http://res.cloudinary.com/demo/image/upload/sample.jpg"> \
             ';
-      setFixtures(input)
-      new ResponsiveImageLoader().loadImages();
 
-      expect($(input).find('.ri').length).toEqual(1);
-      var images = $(input).find('.ri');
-      expect(images[0].src).toEqual("http://res.cloudinary.com/demo/image/upload/sample.jpg");
-    });
+  it("loads images with default settings", function () {
+    window.devicePixelRatio = 0.4;
 
-    it("loads images for retina display", function () {
-      window.devicePixelRatio = 1.5;
-      var input = document.createElement('div');
-      input.innerHTML = '<img class="ri" data-desktop-width="700" data-tablet-width="400" data-phone-width="200" \
-          data-src-desktop="http://res.cloudinary.com/demo/image/upload/sample.jpg"\
-          data-src-tablet="http://res.cloudinary.com/demo/image/upload/sample.jpg"\
-          data-src-phone="http://res.cloudinary.com/demo/image/upload/sample.jpg"> \
-            ';
-      setFixtures(input)
-      new ResponsiveImageLoader({imageServer: "Scene7"}).loadImages();
+    window.innerWidth = 780;
+    document.documentElement.appendChild(input);
 
-      expect($(input).find('.ri').length).toEqual(1);
-      var images = $(input).find('.ri');
-      expect(images[0].src).toEqual("http://res.cloudinary.com/demo/image/upload/sample.jpg?wid=1400");
-    });
+    new ResponsiveImageLoader().loadImages();
+    console.log(document.getElementsByClassName("ri"))
+    expect(document.getElementsByClassName("ri").length).toEqual(1);
+    var images = $(input).find('.ri');
+    expect(images[0].src).toEqual("http://res.cloudinary.com/demo/image/upload/sample.jpg");
+  });
 
-    it("load images with configured breakpoints and imageServer", function () {
-      window.devicePixelRatio = 0.4;
-      var input = document.createElement('div');
-      input.innerHTML = '<img class="ri" data-desktop-width="700" data-phone-width="400" \
-          data-src-desktop="http://res.cloudinary.com/demo/image/upload/sample.jpg"\
-          data-src-phone="http://res.cloudinary.com/demo/image/upload/sample.jpg">';
+  it("loads images for retina display", function () {
+    window.devicePixelRatio = 1.5;
+    window.innerWidth = 780;
 
-      setFixtures(input);
+    document.documentElement.appendChild(input);
 
-      new ResponsiveImageLoader({imageServer: "Cloudinary",
-        breakpoints: new Breakpoints({
-          "desktop": { minWidth: 769, maxWidth: screen.availWidth },
-          "phone": { minWidth:0, maxWidth: 768 }
-        })
-      }).loadImages();
+    new ResponsiveImageLoader({imageServer: "Scene7"}).loadImages();
 
-      expect($(input).find('.ri').length).toEqual(1);
-      var images = $(input).find('.ri');
-      expect(images[0].src).toEqual("http://res.cloudinary.com/demo/image/upload/w_700/sample.jpg");
-    });
+    expect($(input).find('.ri').length).toEqual(1);
+    var images = $(input).find('.ri');
+    expect(images[0].src).toEqual("http://res.cloudinary.com/demo/image/upload/sample.jpg?wid=1400");
+  });
 
-    it("load images depending on the window width", function(){
-      window.devicePixelRatio = 0.4;
-      var input = document.createElement('div');
-      input.innerHTML = '<img class="ri" data-desktop-width="700" data-tablet-width="400" data-phone-width="200" \
-          data-src-desktop="http://res.cloudinary.com/demo/image/upload/sample.jpg"\
-          data-src-tablet="http://res.cloudinary.com/demo/image/upload/sample.jpg"\
-          data-src-phone="http://res.cloudinary.com/demo/image/upload/sample.jpg"> \
-            ';
-      var images = $(input).find('.ri');
-      setFixtures(input);
+  it("load images with configured breakpoints and imageServer", function () {
+    window.devicePixelRatio = 0.4;
+    window.innerWidth = 400;
 
-      new ResponsiveImageLoader({imageServer: "Scene7"}).loadImages();
-      expect(images[0].src).toEqual("http://res.cloudinary.com/demo/image/upload/sample.jpg?wid=700");
+    document.documentElement.appendChild(input);
 
-      window.innerWidth = 100;
+    new ResponsiveImageLoader({imageServer: "Cloudinary",
+      breakpoints: new Breakpoints({
+        "desktop": { minWidth: 769, maxWidth: screen.availWidth },
+        "phone": { minWidth: 0, maxWidth: 768 }
+      })
+    }).loadImages();
 
-      new ResponsiveImageLoader({imageServer: "Scene7"}).loadImages();
-      expect(images[0].src).toEqual("http://res.cloudinary.com/demo/image/upload/sample.jpg?wid=200");
-    });
-
+    expect($(input).find('.ri').length).toEqual(1);
+    var images = $(input).find('.ri');
+    expect(images[0].src).toEqual("http://res.cloudinary.com/demo/image/upload/w_200/sample.jpg");
   });
 });
